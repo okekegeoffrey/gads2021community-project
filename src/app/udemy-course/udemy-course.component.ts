aba2953fdbb9a926eaa140a3fee4cc15d7032e4c
geoffreyokeke <cokekegeoffrey@gmail.com>
2021-12-20 23:10:15 +0100

    trying to wire up the next and prev buttons

--- src/app/udemy-course/udemy-course.component.ts
+++ src/app/udemy-course/udemy-course.component.ts
@@ -8,12 +8,23 @@ import { UdemyService } from '../udemy.service';
 })
 export class UdemyCourseComponent implements OnInit {
 
+  udemyData:any;
+  navData:any;
+
+  // fontawesome
+
   title:any = 'page for udemy courses works!';
 
   constructor(private _udemyService: UdemyService) { }
 
   ngOnInit(): void {
-    this._udemyService.getCourses().subscribe(data => console.log(JSON.stringify(data)));
+    this._udemyService.getCourses().subscribe(data => {console.log(data);
+      const dataNew:any = data;
+      this.udemyData = dataNew.results});
+  }
+
+  ngOnDestroy(): void {
+    this.udemyData.unsubscribe();
   }
 
 }
